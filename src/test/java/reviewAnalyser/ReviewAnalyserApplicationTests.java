4dae452231c48fb8ee9ad0660a8bb3f26e0e5210
gregorytrent11 <gregory.trent11@gmail.com>
2021-02-06 03:23:02 +0000

    changes commit

--- src/test/java/reviewAnalyser/ReviewAnalyserApplicationTests.java
+++ src/test/java/reviewAnalyser/ReviewAnalyserApplicationTests.java
@@ -1,16 +1,13 @@
-package com.simplilearn.ReviewAnalyser;
+package reviewAnalyser;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ReviewAnalyserApplicationTests {
 
-private ReviewAnalyserApplication analyser = new ReviewAnalyserApplication();
-@Test	
-public void testWordCount() {     	
-	 assertEquals(7,analyser.getWordCount("Train to win in the digital economy"));	
-}
-}
+	@Test
+	void contextLoads() {
+	}
 
+}
